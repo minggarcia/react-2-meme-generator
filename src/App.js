9b77dc6e2eafa6ad6ede26ddb2a7969dac11ff52
Charmaine Garcia <garciacharmaine95@gmail.com>
2022-01-20 13:56:10 +0100

    Create new project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import InputForTopAndBottomText from './InputForTopAndBottomText';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <header className="App-header">
+          <h1>Meme Generator</h1>
+        </header>
+      </div>
+      <div>
+        <InputForTopAndBottomText />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/InputForTopAndBottomText.js
@@ -0,0 +1,19 @@
+// import { useState } from 'react';
+
+export default function InputForTopAndBottomText() {
+  // const [topText, setTopText] = useState('');
+  // const [bottomText, setBottomText] = useState('');
+
+  return (
+    <form>
+      <label>
+        Top Text:
+        <input />
+      </label>
+      <label>
+        Bottom Text:
+        <input />
+      </label>
+    </form>
+  );
+}
