279de0d06c4bc41efd59dc079b4bfa08aa74cd29
Charmaine Garcia <garciacharmaine95@gmail.com>
2022-04-19 18:24:36 +0200

    Delete file input file put everything in one component instead

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -1,63 +1,103 @@
-// import { useEffect, useState } from 'react';
-
 import { useEffect, useState } from 'react';
-import TopAndBottomInput from './TopAndBottomInput';
-
-// export default function MemeGenerator() {
-// const [templates, setTemplates] = useState([])
-
-// // Fetch all templates from api:
-// useEffect(() => {
-//   fetch('https://api.memegen.link/templates/').then((res) => res.json().then((res) ))
-// })
-
-// // useEffect(() => {
-// //   fetch('https://api.memegen.link/templates/').then((res) =>
-// //     res
-// //       .json()
-// //       .then((res) => {
-// //         const memes = res.data.memes;
-// //         setMemes(memes);
-// //       })
-// //       .catch((err) => {
-// //         console.error(err);
-// //       }),
-// //   );
-// // }, []);
-
-// function fetchMemes() {
-//   return fetch('https://api.memegen.link/templates/')
-//     .then((response) => response.json())
-//     .then((data) => console.log(data));
-// }
-
-// useEffect(() => {
-//   fetchMemes();
-// }, []);
 
 export default function MemeGenerator() {
   const [template, setTemplate] = useState([]);
+  const [memeImage, setMemeImage] = useState('');
+  const [topText, setTopText] = useState('top text');
+  const [bottomText, setBottomText] = useState('bottom text');
+  const [url, setUrl] = useState('https://api.memegen.link/images/ds.png');
 
   // fetch all templates api
-
   useEffect(() => {
-    const templateToFetch = async () => {
-      try {
-        const response = await fetch('https://api.memegen.link/templates/');
-        const json = await response.json();
-        setTemplate(json);
-      } catch (e) {
-        console.error(e);
-      }
-    };
-    templateToFetch().catch((error) => {
-      console.error(error);
-    });
+    fetch('https://api.memegen.link/templates/')
+      .then((response) => response.json())
+      .then((data) => {
+        const response = data.map((meme) => {
+          return meme.id;
+        });
+        setTemplate(response);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
   }, []);
+
+  // download handler
+
+  function handleDownloadClick() {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    void fetch(setUrl).then((response) => {
+      response
+        .arrayBuffer()
+        .then((buffer) => {
+          const element = document.createElement('a');
+          const file = new Blob([buffer], { type: 'image/jpeg' });
+          element.href = URL.createObjectURL(file);
+          element.download = 'image.jpg';
+          element.click();
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    });
+  }
   return (
     <div>
       <h1>Meme Generator</h1>
-      <TopAndBottomInput />
+      <div>
+        <div>
+          <label>
+            Top Text:
+            <br />
+            <input
+              onChange={(event) => {
+                setTopText(event.currentTarget.value);
+              }}
+              value={topText}
+            />
+          </label>
+          <br />
+          <div>
+            <label>
+              Bottom Text:
+              <br />
+              <input
+                onChange={(event) => {
+                  setBottomText(event.currentTarget.value);
+                }}
+                value={bottomText}
+              />
+            </label>
+          </div>
+        </div>
+      </div>
+
+      <label>
+        Meme Templates
+        <select
+          onChange={(event) => {
+            setMemeImage(event.currentTarget.value);
+          }}
+        >
+          {template.map((id) => (
+            <option key={id} value={id}>
+              {id}
+            </option>
+          ))}
+        </select>
+      </label>
+      <div css={url}>
+        <img
+          width="200px"
+          height="200px"
+          data-test-id="meme-image"
+          src={`https://api.memegen.link/images/${memeImage}/${topText}/${bottomText}.jpg`}
+          alt="meme"
+        />
+      </div>
+      <div>
+        <button onClick={handleDownloadClick}>Download</button>
+      </div>
     </div>
   );
 }
